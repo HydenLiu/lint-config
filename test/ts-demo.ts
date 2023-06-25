026731101e5e5308b09cf8cf1017213fab070235
LHD <sunupdong@gmail.com>
2023-06-25 15:35:27 +0800

    feat: add test

---
+++ test/ts-demo.ts
@@ -0,0 +1,5 @@
+function getData() {
+  return 1
+}
+
+getData()
---
+++ test/vue3.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      count: 0,
+      message: 'Welcome to my counter!'
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    test() {
+      this.count++
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ message }}</h1>
+    <button @click="increment">
+      Increment
+    </button>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 4rem;
+
+  }
+
+  button {
+    padding: 1rem;
+    margin-top: 2rem;
+    font-size: 2rem;
+  }
+</style>
